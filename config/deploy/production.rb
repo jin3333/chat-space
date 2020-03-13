f0b7f8acfa600d86e098bc34e77b8412bceef842
jin3333 <61690420+jin3333@users.noreply.github.com>
2020-03-13 19:11:43 +0900

    edit
    edit production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '13.231.84.132', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -7,7 +7,7 @@ server '13.231.84.132', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+server '13.231.84.132', user: 'ec2-user', roles: %w{app db web}
 
 
 # role-based syntax
